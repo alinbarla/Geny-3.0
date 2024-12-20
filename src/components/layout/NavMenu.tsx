a6053610d2055da2e1afff46e93cc261618d7b79
Alin Barla <alinbarla@hotmail.com>
2024-12-20 14:32:19 -0500

    Update NavMenu.tsx

--- src/components/layout/NavMenu.tsx
+++ src/components/layout/NavMenu.tsx
@@ -24,8 +24,8 @@ export const NavMenu: React.FC<NavMenuProps> = ({ onItemClick }) => {
             className="flex items-center gap-2 px-4 py-2 text-gray-600 hover:text-purple-600 hover:bg-purple-50 rounded-xl transition-colors"
           >
             <div className="relative">
-              <Sparkles className="w-5 h-5" />
-              <Sparkles className="w-3 h-3 text-pink-500 absolute -top-1 -right-1" />
+              <QRCode className="w-5 h-5" />
+              <Wand2 className="w-3 h-3 text-pink-500 absolute -top-1 -right-1" />
             </div>
             <span className="font-medium">QR Code Geny</span>
           </Link>
