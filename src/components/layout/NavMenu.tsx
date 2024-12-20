da9eb253a3b7da0c01e0c28c54573ce8921fd714
Alin Barla <alinbarla@hotmail.com>
2024-12-20 14:39:54 -0500

    Update NavMenu.tsx

--- src/components/layout/NavMenu.tsx
+++ src/components/layout/NavMenu.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { QrCode, Sparkles } from 'lucide-react';
+import { QrCode, Sparkles, Wand2 } from 'lucide-react';
 import { Link } from 'react-router-dom';
 
 interface NavMenuProps {
